d7f5810bce9f6815ab7db4f0a4b0c1e8a6f780f3
AleksSenkou <aleksey.senkou@gmail.com>
2014-10-14 17:14:33 +0300

    fucking shoes

--- task 1/wrong_man.rb
+++ task 1/wrong_man.rb
@@ -5,48 +5,56 @@ Shoes.app height: 750, width: 800 do
     stroke midnightblue
     fill midnightblue
     rect 0, 700, 800, 200
-    -10.step(790, 200) { |i| arc i, 680, 200, 100, 0, 10 }	
+    -10.step(790, 200) { |i| arc i, 680, 200, 100, 0, 10 }  
   end
 
   def mountain
     stroke sienna
     fill sienna
     rect 670, 100, 130, 700
-    rect 600, 100, 70, 35		
+    rect 600, 100, 70, 35   
   end
 
   def cable
     stroke black
     fill black
     strokewidth 2
-    line 600, 100, 600, 200		
+    line 600, 100, 600, 200 
   end
 
   def world
-    sea()
-    mountain()
-    cable()
+    stroke white
+    fill white
+    rect 0, 0, 800, 800
+    sea
+    mountain
+    cable
   end
 
   def man(x, y, color)
-    strokewidth 0
+    nostroke
     fill goldenrod
     oval x - 26, y - 20, 11
     oval x + 4, y - 20, 11
     oval x - 10, y - 39, 10
-    rect x - 15, y + 20, 11, 30  	
-    rect x + 4, y + 20, 11, 30  	
+    rect x - 15, y + 20, 11, 30   
+    rect x + 4, y + 20, 11, 30    
     fill white
     oval x - 20, y - 16, 7
     oval x + 7, y - 16, 7
     fill color
     rect x - 15, y - 20, 30, 40  
     fill black
-    arc x - 21, y + 48, 18, 7, 0, 10	
-    arc x + 3, y + 48, 18, 7, 0, 10	
+    arc x - 21, y + 48, 18, 7, 0, 10  
+    arc x + 3, y + 48, 18, 7, 0, 10 
   end
 
-  world()
-  man(600, 200, red) 
+  def write_text(whiсh_man, text)
+    para "#{whiсh_man.capitalize} man:"
+    para "<i> #{text} </i>",margin_left: 20, undercolor: whiсh_man, underline: 'double', size: 16
+  end
+
+  world
+  man(600, 200, red)
   man(700, 47, green)
 end
