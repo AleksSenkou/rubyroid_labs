c4bcc13bfb39d62b24ac9c191b4b029ef2b15ebc
AleksSenkou <aleksey.senkou@gmail.com>
2014-10-30 17:32:44 +0300

    update

--- task_3_simple/bot.rb
+++ task_3_simple/bot.rb
@@ -1,7 +1,15 @@
 require "mechanize"
 require "colorize"
+POINTS = "." * 80
+TABS = " " * 5
+COUNT = 5
 
 bot = Mechanize.new
 page = bot.get('http://bashorg.org/')
 
-puts "\nPage title: " + page.title.green.underline
+puts TABS + page.title.green.on_black.underline
+puts "Quotes: \n"
+page.search(".quote").each_with_index do |quote, index|
+  puts (index + 1).to_s.green + POINTS.green  
+  puts quote.text
+end
