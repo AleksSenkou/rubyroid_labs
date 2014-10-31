1990d0a4ac3902539b6109c1c60529b78b6b2e61
AleksSenkou <aleksey.senkou@gmail.com>
2014-10-31 13:11:31 +0300

    almost ready

--- task_3_simple/bot.rb
+++ task_3_simple/bot.rb
@@ -1,15 +1,30 @@
 require "mechanize"
 require "colorize"
-POINTS = "." * 80
+POINTS = "." * 65
 TABS = " " * 5
-COUNT = 5
+COUNT = 7
+
+def parsing_page(page)
+  puts TABS + page.title.green.on_black.underline
+  puts "Quotes:"
+  
+  quotes = parsing_quotes(page)
+  page.search(".vote").search('a[1]').each_with_index do |citat, index|
+    break if index >= COUNT
+    puts citat.text.green + POINTS.green
+    puts quotes[index]
+  end
+end
+
+def parsing_quotes(page)
+  quotes = []
+  page.search(".quote").each_with_index do |quote, index|
+    index < COUNT ? quotes << quote.text : break
+  end
+  quotes
+end
 
 bot = Mechanize.new
 page = bot.get('http://bashorg.org/')
+parsing_page(page)
 
-puts TABS + page.title.green.on_black.underline
-puts "Quotes: \n"
-page.search(".quote").each_with_index do |quote, index|
-  puts (index + 1).to_s.green + POINTS.green  
-  puts quote.text
-end
