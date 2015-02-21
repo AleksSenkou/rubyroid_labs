7c5365bf30c6e8b5bb20a5ed3abf0ebab95c95bd
alekssenkou <aleksey.senkou@gmail.com>
2015-02-21 21:39:56 +0300

    canvas onclick is enable

--- task_6_sinatra_life_game/public/js/index.js
+++ task_6_sinatra_life_game/public/js/index.js
@@ -1,6 +1,37 @@
 //require("canvas.js");
 
 $(document).ready(function() {
+  var Life = {
+    DEAD: 0,
+    ALIVE: 1,
+    STOPPED: 0,
+    RUNNING : 1
+  };
+
+  Life.gridInit = function(x, y, state) {
+      var matrix = [];
+      for(var i = 0; i < y; i++) {
+        matrix[i] = [];
+        for(var j = 0; j < x; j++) {
+          matrix[i][j] = state;
+        }
+      }
+      return matrix;
+  };
+          
+  var Cell = {
+    count_x: 40, 
+    count_y: 20,
+    position_x: 0,
+    position_y: 0 
+  }
+
+  Life.grid = Life.gridInit(Cell.count_y, Cell.count_x, 0);
+
+///////////////////////////////////////////////////////////
+////////////////// WORK WITH ANIMATION ////////////////////
+///////////////////////////////////////////////////////////
+
   drawingCanvas = document.getElementById("canvas");
   context = drawingCanvas.getContext("2d");
   
@@ -8,32 +39,68 @@ $(document).ready(function() {
     size_x: parseInt(drawingCanvas.getAttribute("width")),
     size_y: parseInt(drawingCanvas.getAttribute("height"))
   }
-  var Cell = {
-    count_x: 40, 
-    count_y: 20 
-  }
+
   Cell.size_x = Canvas.size_x / Cell.count_x;
   Cell.size_y = Canvas.size_y / Cell.count_y;
   
-  Canvas.drawGrid = function() {
+  Canvas.drawGridLines = function() {
     context.lineWidth = 1;
-    context.strokeStyle = "DimGrey";
-
-    for(var i = Cell.size_x; i <= this.size_x - Cell.size_x; i += Cell.size_x) {
+    context.strokeStyle = "#000000";
+    
+    for(var i = 0; i <= this.size_x; i += Cell.size_x) {
       context.moveTo(i, 0);
-      context.lineTo(i, this.size_y);
+      context.lineTo(i, this.size_y);  
       context.stroke();
     }
   
-    for(i = Cell.size_y; i <= this.size_y - Cell.size_y; i += Cell.size_y) {
+    for(i = 0; i <= this.size_y; i += Cell.size_y) {
       context.moveTo(0, i);
       context.lineTo(this.size_x, i);
       context.stroke();
+    }    
+  };
+        
+  Canvas.updateGridCells = function() {
+    for(var i = 0; i <= Cell.count_y; i++) {
+      for(var j = 0; j <= Cell.count_x; j++) {
+        if (Life.grid[i][j] == Life.ALIVE) {
+          context.fillStyle = "#4169E1";                 
+        } else {
+          context.fillStyle = "#eee";
+        }
+        context.fillRect(j * Cell.size_x + 1, i * Cell.size_y + 1,
+                           Cell.size_x - 1,     Cell.size_y - 1);
+      }
     }
-  }
+  };
 
+  function canvasOnClickHandler(event) {
+    getCursorPosition(event);
+    var state = Life.grid[Cell.position_y][Cell.position_x] == Life.ALIVE ? Life.DEAD : Life.ALIVE;
+    Life.grid[Cell.position_y][Cell.position_x] = state;
+    Canvas.updateGridCells();
+  };
+    
+  function getCursorPosition(event) {
+    var x;
+    var y;
+    if (event.pageX || event.pageY) {
+      x = event.pageX;
+      y = event.pageY;
+    } else {
+      x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+      y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+    }
+  
+    x -= drawingCanvas.offsetLeft;
+    y -= drawingCanvas.offsetTop;
+    
+    Cell.position_y = Math.floor((y - 4) / Cell.size_y);
+    Cell.position_x = Math.floor((x - 2) / Cell.size_x);
+  };
+          
+    Canvas.drawGridLines();
+    drawingCanvas.addEventListener("click", canvasOnClickHandler, false);
 
-  Canvas.drawGrid();
-  alert(Cell.size_x);
-})
+});
 
